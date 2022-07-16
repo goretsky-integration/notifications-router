1582ddc1517f6d92b65aba15564b85c851192ac6
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-16 20:06:05 +0600

    :wrench: Setup config

--- src/config.py
+++ src/config.py
@@ -6,10 +6,9 @@ __all__ = (
     'ROOT_PATH',
     'LOGS_FILE_PATH',
     'TELEGRAM_BOT_TOKEN',
-    'IS_DEBUG',
-    'RABBITMQ_HOST',
-    'RABBITMQ_PORT',
-    'RABBITMQ_EXCHANGE',
+    'DEBUG',
+    'RABBITMQ_URL',
+    'MONGODB_URL',
 )
 
 env = Env()
@@ -19,7 +18,6 @@ ROOT_PATH = pathlib.Path(__file__).parent.parent
 LOGS_FILE_PATH = ROOT_PATH / 'logs.log'
 
 TELEGRAM_BOT_TOKEN: str = env.str('TELEGRAM_BOT_TOKEN')
-IS_DEBUG: bool = env.bool('IS_DEBUG')
-RABBITMQ_HOST: str = env.str('RABBITMQ_HOST')
-RABBITMQ_PORT: int = env.int('RABBITMQ_PORT')
-RABBITMQ_EXCHANGE: str = env.str('RABBITMQ_EXCHANGE')
+MONGODB_URL: str = env.str('MONGODB_URL')
+DEBUG: bool = env.bool('DEBUG')
+RABBITMQ_URL: str = env.str('RABBITMQ_URL')
