b7db60c44e28933b95ce69527787bdcb8b79b3e4
usbtypec1 <eldos.baktybekov@gmail.com>
2022-07-19 13:15:25 +0600

    :sparkles: Add alias to send messages batch

--- src/telegram.py
+++ src/telegram.py
@@ -1,21 +1,23 @@
+from typing import Iterable
+
 import httpx
 
-import config
-import views
+from utils import logger
 
 __all__ = (
-    'Telegram',
-    'send_message',
+    'send_messages',
+    'TelegramSender',
 )
 
 
-class Telegram:
+class TelegramSender:
+    __slots__ = ('_send_message_url', '_token')
 
     def __init__(self, token: str):
         self._token = token
-        self._api_base_url = f'https://api.telegram.org/bot{self._token}'
+        self._send_message_url = f'https://api.telegram.org/bot{self._token}/sendMessage'
 
-    def send_message(self, chat_id: int | str, text: str) -> bool:
+    def send_message(self, chat_id: int, text: str) -> bool:
         """Send message to telegram.
 
         Args:
@@ -26,7 +28,7 @@ class Telegram:
             True if ok.
         """
         response = httpx.post(
-            url=f'{self._api_base_url}/sendMessage',
+            url=self._send_message_url,
             json={
                 'text': text,
                 'chat_id': chat_id,
@@ -36,15 +38,10 @@ class Telegram:
         return response.json()['ok']
 
 
-def send_message(chat_id: int | str, message_view: views.MessageView) -> bool:
-    """Send message to telegram
-
-    Args:
-        chat_id: chat id or username.
-        message_view: report message
-
-    Returns:
-        True on success.
-    """
-    bot = Telegram(config.TELEGRAM_BOT_TOKEN)
-    return bot.send_message(chat_id, message_view.as_text())
+def send_messages(bot: TelegramSender, text: str, chat_ids: Iterable[int]):
+    for chat_id in chat_ids:
+        is_message_sent = bot.send_message(chat_id, text)
+        if is_message_sent:
+            logger.debug(f'Message has been sent')
+        else:
+            logger.warning(f'Message has not been sent')
