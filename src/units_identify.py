392f92d40212d905bba977bec183f06a71b16d5e
Eldos <eldos.baktybekov@gmail.com>
2022-10-14 15:45:31 +0600

    :recycle: Refactor grouper to pydantic model

--- src/units_identify.py
+++ src/units_identify.py
@@ -4,11 +4,10 @@ from typing import Iterable
 import models
 
 
-def group_chat_ids_by_unit_id(reports: Iterable[models.ReportFromMongoDB]) -> dict[int, set[int]]:
-    unit_id_to_chat_ids = collections.defaultdict(set)
-    for report in reports:
-        for unit_id in report['unit_ids']:
+def group_chat_ids_by_unit_id(chats_to_retranslate: Iterable[models.ChatToRetranslate]) -> dict[int, set[int]]:
+    unit_id_to_chat_ids: dict[int, set[int]] = collections.defaultdict(set)
+    for chat_to_retranslate in chats_to_retranslate:
+        for unit_id in chat_to_retranslate.unit_ids:
             chat_ids = unit_id_to_chat_ids[unit_id]
-            chat_ids.add(report['chat_id'])
-            unit_id_to_chat_ids[unit_id] = chat_ids
+            chat_ids.add(chat_to_retranslate.chat_id)
     return unit_id_to_chat_ids
