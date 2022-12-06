0e1af1ed81a54cc1f7b22ce5b421a2e535d2fa8b
Eldos <eldos.baktybekov@gmail.com>
2022-12-06 14:12:03 +0600

    :white_check_mark: Add tests for database api

---
+++ tests/test_db_api.py
@@ -0,0 +1,21 @@
+import httpx
+
+import models
+from db_api import DatabaseAPI
+
+
+class MockResponse:
+
+    def json(self):
+        return [models.ChatToRetranslate(chat_id=1234, unit_ids=(123, 234, 345))]
+
+
+def mock_get(*args, **kwargs) -> MockResponse:
+    return MockResponse()
+
+
+def test_database_api_get_chats_to_retranslate(monkeypatch):
+    monkeypatch.setattr(httpx, 'get', mock_get)
+    database_api = DatabaseAPI('gdfjgosjifjsdiofj')
+    reports = database_api.get_chats_to_retranslate('STATISTICS')
+    assert reports == [models.ChatToRetranslate(chat_id=1234, unit_ids=(123, 234, 345))]
