1ae999e4e1bed11f86ea4c4f1f4cfa63dfd1b1d9
Eldos <eldos.baktybekov@gmail.com>
2023-02-14 10:26:15 +0600

    :alien: GI-17 Use other endpoint to get report routes

--- src/db_api.py
+++ src/db_api.py
@@ -10,9 +10,10 @@ class DatabaseAPI:
     __slots__ = ('__base_url',)
 
     def __init__(self, base_url: str):
-        self.__base_url = base_url
+        self.__base_url = base_url.rstrip('/')
 
-    def get_chats_to_retranslate(self, report_type: str) -> list[models.ChatToRetranslate]:
-        url = f'{self.__base_url.rstrip("/")}/reports/retranslate/{report_type}/'
-        response = httpx.get(url)
+    def get_report_routes(self, report_type: str) -> list[models.ChatToRetranslate]:
+        request_query_params = {'report_type': report_type}
+        url = f'{self.__base_url}/reports/'
+        response = httpx.get(url, params=request_query_params)
         return parse_obj_as(list[models.ChatToRetranslate], response.json())
