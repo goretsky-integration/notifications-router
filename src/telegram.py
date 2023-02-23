edfe4de2a404c3f725d2c3cb283436611556b710
Eldos <eldos.baktybekov@gmail.com>
2023-02-23 10:11:08 +0600

    :recycle: GI-17 Raise exception on error

--- src/telegram.py
+++ src/telegram.py
@@ -1,8 +1,9 @@
 from typing import Iterable
 
 import httpx
-
 from loguru import logger
+
+import exceptions
 
 __all__ = ('TelegramSender',)
 
@@ -14,15 +15,15 @@ class TelegramSender:
         self._token = token
         self._send_message_url = f'https://api.telegram.org/bot{self._token}/sendMessage'
 
-    def send_message(self, chat_id: int, text: str) -> bool:
+    def send_message(self, chat_id: int, text: str) -> None:
         """Send message to telegram.
 
         Args:
             chat_id: chat id to send (telegram id or username).
             text: message to send.
 
-        Returns:
-            True if ok.
+        Raises:
+            TelegramAPIError on error with description.
         """
         response = httpx.post(
             url=self._send_message_url,
@@ -32,12 +33,16 @@ class TelegramSender:
                 'parse_mode': 'html',
             }
         )
-        return response.json()['ok']
+        response_data = response.json()
+        if not response_data['ok']:
+            error_description = response_data.get('description')
+            raise exceptions.TelegramAPIError(error_description)
 
     def send_messages(self, text: str, chat_ids: Iterable[int]):
         for chat_id in chat_ids:
-            is_message_sent = self.send_message(chat_id, text)
-            if is_message_sent:
-                logger.debug(f'Message has been sent to {chat_id}')
+            try:
+                self.send_message(chat_id, text)
+            except exceptions.TelegramAPIError as error:
+                logger.warning(f'Could not send message to {chat_id}. Error {error.error_description}')
             else:
-                logger.warning(f'Message has not been sent to {chat_id}')
+                logger.info(f'Message sent to {chat_id}')
