4e9d937f34ee44859c97818e21c75dbaf21fd87d
Eldos <eldos.baktybekov@gmail.com>
2023-11-10 09:35:08 +0600

    :wrench: Update config loader file
    - Use `tomllib` for `.toml` instead of `configparser` and old `.ini` format.
    - Remove unused fields in config.

--- src/config.py
+++ src/config.py
@@ -1,11 +1,9 @@
 import pathlib
-from configparser import ConfigParser
+import tomllib
 from dataclasses import dataclass
 
 __all__ = (
-    'ROOT_PATH',
-    'LOGS_FILE_PATH',
-    'load_config',
+    'load_config_from_file',
     'Config',
 )
 
@@ -13,26 +11,13 @@ __all__ = (
 @dataclass(frozen=True, slots=True)
 class Config:
     telegram_bot_token: str
-    debug: bool
-    rabbitmq_url: str
-    database_api_url: str
-    event_max_lifetime_in_seconds: int
+    redis_url: str
 
 
-def load_config(config_file_path: str | pathlib.Path) -> Config:
-    config_parser = ConfigParser()
-    config_parser.read(config_file_path)
-
-    app_config = config_parser['app']
-
+def load_config_from_file(file_path: pathlib.Path) -> Config:
+    config_text = file_path.read_text(encoding='utf-8')
+    config = tomllib.loads(config_text)
     return Config(
-        telegram_bot_token=app_config.get('telegram_bot_token'),
-        debug=app_config.getboolean('debug'),
-        rabbitmq_url=app_config.get('rabbitmq_url'),
-        database_api_url=app_config.get('database_api_url'),
-        event_max_lifetime_in_seconds=app_config.getint('event_max_lifetime_in_seconds'),
+        telegram_bot_token=config['telegram']['bot_token'],
+        redis_url=config['redis']['url'],
     )
-
-
-ROOT_PATH = pathlib.Path(__file__).parent.parent
-LOGS_FILE_PATH = ROOT_PATH / 'logs.log'
